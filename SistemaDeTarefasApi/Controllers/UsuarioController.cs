52341089cbbbc65e520575beea0394ba7baac91b
Leonardo <lleonardo1555@gmail.com>
2023-02-13 21:41:59 -0300

    Configuração context

---
+++ SistemaDeTarefasApi/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeTarefasApi.Models;
+
+namespace SistemaDeTarefasApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return Ok();
+        }
+    }
+}
