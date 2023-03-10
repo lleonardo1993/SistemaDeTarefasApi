43a00f6661ccdd90694688cc9c880bd620e0b020
Leonardo <lleonardo1555@gmail.com>
2023-03-09 22:21:03 -0300

    Migration e Repositório finalizado

---
+++ SistemaDeTarefasApi/Repositorios/Interfaces/ITarefaRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using SistemaDeTarefasApi.Models;
+
+namespace SistemaDeTarefasApi.Repositorios.Interfaces
+{
+    public interface ITarefaRepositorio
+    {
+        Task<List<TarefaModel>> BuscarTodasTarefas();
+        Task<TarefaModel> BuscarPorId(int id);
+        Task<TarefaModel> Adicionar(TarefaModel tarefa);
+        Task<TarefaModel> Atualizar(TarefaModel tarefa, int id);
+        Task<bool> Apagar(int id);
+    }
+}
---
+++ SistemaDeTarefasApi/Repositorios/TarefaRepositorio.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefasApi.Data;
+using SistemaDeTarefasApi.Models;
+using SistemaDeTarefasApi.Repositorios.Interfaces;
+
+namespace SistemaDeTarefasApi.Repositorios
+{
+    public class TarefaRepositorio : ITarefaRepositorio
+    {
+        private readonly SistemaTarefasDBContext _dbContext;
+        public TarefaRepositorio(SistemaTarefasDBContext sistemaTarefasDBContext)
+        {
+            _dbContext = sistemaTarefasDBContext;
+        }
+
+        public async Task<TarefaModel> BuscarPorId(int id)
+        {
+            return await _dbContext.Tarefas.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<TarefaModel>> BuscarTodasTarefas()
+        {
+            return await _dbContext.Tarefas.ToListAsync();
+        }
+        public async Task<TarefaModel> Adicionar(TarefaModel tarefa)
+        {
+            await _dbContext.Tarefas.AddAsync(tarefa);
+            await _dbContext.SaveChangesAsync();
+
+            return tarefa;
+        }
+        public async Task<TarefaModel> Atualizar(TarefaModel tarefa, int id)
+        {
+            TarefaModel tarefaPorId = await BuscarPorId(id);
+            
+            if(tarefaPorId == null)
+            {
+                throw new Exception($"Tarefa para o ID: {id} não foi encontrada no banco de dados.");
+            }
+
+            tarefaPorId.Nome = tarefa.Nome;
+            tarefaPorId.Descricao = tarefa.Descricao;
+            tarefaPorId.Status = tarefa.Status;
+            tarefaPorId.UsuarioId = tarefa.UsuarioId;
+
+            _dbContext.Tarefas.Update(tarefaPorId);
+            await _dbContext.SaveChangesAsync();
+
+            return tarefaPorId;
+        }
+
+        public async Task<bool> Apagar(int id)
+        {
+            TarefaModel tarefaPorId = await BuscarPorId(id);
+
+            if (tarefaPorId == null)
+            {
+                throw new Exception($"Tarefa para o ID: {id} não foi encontrado no banco de dados.");
+            }
+
+            _dbContext.Tarefas.Remove(tarefaPorId);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }       
+
+    }
+}
